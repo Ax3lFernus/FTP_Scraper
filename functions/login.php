0381eb67a565b86f5e70c2bdc1d5e5deff3d7bf7
Alessandro Annese <a.annese99@gmail.com>
2022-02-23 21:43:20 +0100

    Snellita la procedura di login

--- functions/login.php
+++ functions/login.php
@@ -1,27 +1,26 @@
 <?php
 session_start();
 require __DIR__ . '/functions.php';
-
-header('Content-Type: application/json');
 if (isset($_POST['server']) && isset($_POST['username']) && isset($_POST['password']) && isset($_POST['port']) && isset($_POST['protocol'])) {
     $server =$_POST['server'];
     $username = $_POST['username'];
     $password = $_POST['password'];
     $port = $_POST['port'];
-    $protocol = $_POST['protocol'];
+    $protocol = $_POST['protocol'] == 'true';
 
     $ftp = new \FtpClient\FtpClient();
     try {
-        $ftp->connect($server, $protocol == 'true', $port);
+        $ftp->connect($server, $protocol, $port);
         $ftp->login($username, $password);
         $token = generateRandomString(24);
         $_SESSION['ftp_vars'] = array('server' => $server, 'username' => $username, 'password' => $password, 'port' => $port, 'protocol' => ($protocol == 'true'));
-        echo json_encode("{\"success\": true}");
+        header('Location: /download.php');
         exit();
     } catch (\FtpClient\FtpException $e) {
-        echo json_encode("{\"success\": false, \"error\": \"Credenziali errate.\"}");
+        header('Location: /index.php?error=Credenziali errate.');
     }
 } else {
-    echo json_encode("{\"success\": false, \"error\": \"Non sono stati passati tutti i dati per effettuare l'accesso.\"}");
+    header('Location: /index.php?error=Non sono stati passati tutti i dati per effettuare l\'accesso.');
 }
+session_unset();
 session_destroy();
--- index.php
+++ index.php
@@ -11,43 +11,54 @@ require 'layouts/head.php';
 
 <body class="text-center">
 <main class="form-signin">
-    <form id="login">
+    <form id="login" method="post" action="/functions/login.php">
         <img class="mb-3" src="<?php echo $link . '/assets/images/logo.png'; ?>" alt="" width="100" height="100">
         <h1 class="h3 mb-3 fw-normal">Accedi al server FTP</h1>
         <?php
         if(isset($_GET['error'])) echo "<p class=\"text-danger font-weight-bold\">" . $_GET['error'] . "</p>";
         ?>
         <div class="form-floating mb-3">
-            <select class="form-select" id="protocol">
-                <option selected value="1">FTP</option>
-                <option value="2">FTPS</option>
+            <select class="form-select" id="protocol" name="protocol">
+                <option selected value="false">FTP</option>
+                <option value="true">FTPS</option>
             </select>
             <label for="protocol">Protocollo</label>
         </div>
         <div class="form-floating mb-3">
             <div class="input-group">
-                <input type="text" class="form-control" id="username" placeholder="Username" aria-label="Username" required>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Username" aria-label="Username" required>
                 <span class="input-group-text">@</span>
-                <input type="text" class="form-control" id="server" placeholder="Server" aria-label="Server" required
+                <input type="text" class="form-control" id="server" name="server" placeholder="Server" aria-label="Server" required
                        pattern="^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])|(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$">
             </div>
         </div>
 
         <div class="form-floating mb-3">
-            <input type="password" id="password" placeholder="Password" class="form-control" required>
+            <input type="password" id="password" name="password" placeholder="Password" class="form-control" required>
             <label for="password">Password</label>
         </div>
 
         <div class="form-floating">
-            <input type="number" min="0" max="65535" id="port" class="form-control" value="21" required>
+            <input type="number" min="0" max="65535" id="port" name="port" class="form-control" value="21" required>
             <label for="porta">Numero di porta</label>
         </div>
 
         <button id="form-btn" class="w-100 btn btn-lg btn-primary mt-3" type="submit">Accedi al server</button>
-        <p class="mt-5 mb-3 text-muted">FTP Scraper &copy; <?php echo date('Y');?></p>
     </form>
+    <p class="mt-5 mb-3 text-muted">FTP Scraper &copy; <?php echo date('Y');?></p>
 </main>
 <?php require('layouts/scripts.php'); ?>
-<script src="./assets/js/index.js"></script>
+<script>
+    $("#form-btn").click(_ => {
+        $(".text-danger").hide();
+        $("#login").submit();
+        $("#form-btn").prop("disabled", true).text("Accesso in corso...");
+        $("#username").prop("disabled", true);
+        $("#password").prop("disabled", true);
+        $("#server").prop("disabled", true);
+        $("#port").prop("disabled", true);
+        $("#protocol").prop("disabled", true);
+    });
+</script>
 </body>
 </html>
